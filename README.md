e7452b4dfbb6f2b26f4db12324a9481842396aeb
rajudevolla <rajudevolla2010@gmail.com>
2021-01-16 13:02:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ng2-charts-pie-template-kcr6f1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ng2-charts-pie-template-kcr6f1)
---
+++ src/app/app.component.ts
@@ -0,0 +1,117 @@
+import { Component, OnInit } from "@angular/core";
+import { ChartType, ChartOptions } from "chart.js";
+import "chartjs-plugin-labels";
+import {
+  SingleDataSet,
+  Label,
+  monkeyPatchChartJsLegend,
+  monkeyPatchChartJsTooltip
+} from "ng2-charts";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  // Pie
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+    legend: {
+      display: "true",
+      position: "right",
+      align: "center"
+    },
+    title: {
+      text: "Idea Status",
+      display: true,
+      fontSize: 20,
+      fontColor: "green",
+      align: "center"
+    },
+    plugins: {
+      labels: [
+        {
+          render: "percentage",
+          fontColor: ["red", "red", "red", "red"],
+          position: "outside"
+        }
+      ]
+    }
+  };
+  public chartColors2: Array<any> = [
+    {
+      backgroundColor: ["red", "blue", "green", "yellow"],
+      hoverBackgroundColor: ["#92c0fc", "#fff68a", "#cbffa8", "#a6fffe"],
+      borderWidth: 2
+    }
+  ];
+  public pieChartLabels: Label[] = [
+    "In Progress",
+    "To be Started",
+    "Not Started",
+    "Completed"
+  ];
+  public pieChartData: SingleDataSet = [41, 22, 4, 69];
+  public pieChartType: ChartType = "pie";
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
+
+  //barchart
+  public barChartType: ChartType = "bar";
+  public chartData1: SingleDataSet = [69, 6, 36, 3, 23];
+  public chartLables1: Label[] = [
+    "Automate",
+    "Eliminate",
+    "Enable",
+    "ReUse",
+    "blank"
+  ];
+  public barChartLegend = false;
+  public barChartPlugins = [];
+  public barchartColors2: Array<any> = [
+    {
+      backgroundColor: ["red", "blue", "green", "yellow", "pink"],
+      hoverBackgroundColor: ["#92c0fc", "#fff68a", "#cbffa8", "#a6fffe"],
+      borderWidth: 2
+    }
+  ];
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+    title: {
+      display: true,
+      text: "Idea of Category"
+    },
+    scales: {
+      yAxes: [
+        {
+          gridLines: {
+            offsetGridLines: true
+          },
+          ticks: {
+            suggestedMin: 50,
+            suggestedMax: 80,
+            stepSize: 10
+          }
+        }
+      ]
+    },
+    plugins: {
+      labels: [
+        {
+          render: "value",
+          fontColor: ["red", "red", "red", "red", "red"],
+          position: "center",
+          align: "center"
+        }
+      ]
+    }
+  };
+
+  constructor() {
+    monkeyPatchChartJsTooltip();
+    monkeyPatchChartJsLegend();
+  }
+
+  ngOnInit() {}
+}
